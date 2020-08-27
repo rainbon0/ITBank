58ab3287d6fa43e3e12eb4122796fe4125b4c0d6
rainbon0 <47206696+rainbon0@users.noreply.github.com>
2020-08-27 23:54:42 +0900

    Revert "2020.08.27 14:52 봉찬균"
    This reverts commit be129ea9dad89c19b50bb010cfce52e8846ca3e8.

--- main/src/main/java/com/itbank/dao/ClientDAO.java
+++
@@ -1,9 +0,0 @@
-package com.itbank.dao;
-
-import java.util.List;
-
-import com.itbank.vo.MerchandiseVO;
-
-public interface ClientDAO {
-	List<MerchandiseVO> recommendedHomeList();
-}
--- main/src/main/webapp/resources/js/moveTopBtn.js
+++
@@ -1,6 +0,0 @@
-$('.btn_gotop').click(function() {
-	$('html, body').animate({
-		scrollTop : 0
-	}, 400);
-	return false;
-});
