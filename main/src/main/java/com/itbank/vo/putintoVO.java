592adb54b9a9114852ae3e8cc6bd25926e511ad1
rainbon0 <47206696+rainbon0@users.noreply.github.com>
2020-08-29 09:13:08 +0900

    재시도

---
+++ main/src/main/java/com/itbank/vo/putintoVO.java
@@ -0,0 +1,54 @@
+package com.itbank.vo;
+
+public class putintoVO {
+	private String merchanCode;
+	private String merchanName;
+	private String imgSrc;
+	private String brand;
+	private String category;
+	private String detailSrc;
+	private String price;
+	public String getMerchanCode() {
+		return merchanCode;
+	}
+	public void setMerchanCode(String merchanCode) {
+		this.merchanCode = merchanCode;
+	}
+	public String getMerchanName() {
+		return merchanName;
+	}
+	public void setMerchanName(String merchanName) {
+		this.merchanName = merchanName;
+	}
+	public String getImgSrc() {
+		return imgSrc;
+	}
+	public void setImgSrc(String imgSrc) {
+		this.imgSrc = imgSrc;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getDetailSrc() {
+		return detailSrc;
+	}
+	public void setDetailSrc(String detailSrc) {
+		this.detailSrc = detailSrc;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+}
